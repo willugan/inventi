9368200d2f26175592baba6d4f9824db72c8cf04
logonrm <logonrm@fiap.com.br>
2018-05-17 21:09:26 -0300

    Classes Chatbot, Navegabilidade e Pergunta (LF)

--- src/br/com/inventi/models/Navegabilidade.java
+++ src/br/com/inventi/models/Navegabilidade.java
@@ -1,4 +1,55 @@
 package br.com.inventi.models;
 
 public class Navegabilidade  {
+    private int codNav;
+    private String linkAjuda;
+    private Chatbot chatbot;
+
+    //Construtor vazio
+    public Navegabilidade()
+    {
+        super();
+    }
+
+    //Construtor cheio
+    public Navegabilidade(int codNav, String linkAjuda, Chatbot chatbot) {
+        this.codNav = codNav;
+        this.linkAjuda = linkAjuda;
+        this.chatbot = chatbot;
+    }
+
+    public void setAll(int codNav, String linkAjuda, Chatbot chatbot) {
+        this.codNav = codNav;
+        this.linkAjuda = linkAjuda;
+        this.chatbot = chatbot;
+    }
+
+    public String getAll()
+    {
+        return "Código da navegabilidade....... "+codNav + "\n" +
+               "Links da página................ "+linkAjuda + "\n"
+               +"Chatbot....................... "+ chatbot.getAll();
+    }
+
+    public int getCodNav() {return codNav;}
+
+    public void setCodNav(int codNav) {
+        this.codNav = codNav;
+    }
+
+    public String getLinkAjuda() {
+        return linkAjuda;
+    }
+
+    public void setLinkAjuda(String linkAjuda) {
+        this.linkAjuda = linkAjuda;
+    }
+
+    public Chatbot getChatbot() {
+        return chatbot;
+    }
+
+    public void setChatbot(Chatbot chatbot) {
+        this.chatbot = chatbot;
+    }
 }
--- src/br/com/inventi/models/Pergunta.java
+++ src/br/com/inventi/models/Pergunta.java
@@ -1,4 +1,60 @@
 package br.com.inventi.models;
 
 public class Pergunta {
+    private int codPergunta;
+    private String pergunta;
+    private Usuario usuario;
+
+
+    //Construtor vazio
+    public Pergunta()
+    {
+        super();
+    }
+
+    //Construtor cheio
+    public Pergunta(int codPergunta, String pergunta, Usuario usuario)
+    {
+        this.codPergunta = codPergunta;
+        this.pergunta = pergunta;
+        this.usuario = usuario;
+    }
+
+    public String getAll()
+    {
+        return + codPergunta + "\n"
+            + pergunta + "\n"
+            + usuario.getAll()+ "\n";
+    }
+
+    public void setAll(int codPergunta, String pergunta, Usuario usuario)
+    {
+        this.codPergunta = codPergunta;
+        this.pergunta = pergunta;
+        this.usuario = usuario;
+    }
+
+    public int getCodPergunta() {
+        return codPergunta;
+    }
+
+    public void setCodPergunta(int codPergunta) {
+        this.codPergunta = codPergunta;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
